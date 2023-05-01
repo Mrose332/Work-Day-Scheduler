6259fcfbae64e2d04334de0e1ce2053b15e8ef81
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-05-01 19:51:31 -0400

    update script.js implemented to load save data to local storage each hour

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,25 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss"));
+
+let updateTime = setInterval(function () {
+    let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss")
+    $("#currentDay").text(currentTime)
+}, 1000);
+
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 $(document).ready(function() {
-    
-  function displayTime() {
-  let rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
-  
-})  
+
+      $('.saveBtn').on('click', function() {
+        userEntry = $(this).siblings('.description').val();
+        timeKey = $(this).parent().attr('id');
+        localStorage.setItem(timeKey, userEntry);
+      })})
